193bf44ef2f518700239cff3b6818ef05fed4fb0
irisesther <irisesther122@icloud.com>
2026-07-02 10:04:48 +0200

    Update eco-quiz-mobile.react-native-app.js

--- bit-components/eco-quiz/eco-quiz-mobile/eco-quiz-mobile.react-native-app.js
+++ bit-components/eco-quiz/eco-quiz-mobile/eco-quiz-mobile.react-native-app.js
@@ -1,3 +1,7 @@
+<Image 
+  source={require('../../../../frame.png')} 
+  style={{ width: 220, height: 220, alignSelf: 'center', marginTop: 25, marginBottom: 25 }} 
+/>
 export default {
   name: 'eco-quiz-mobile',
 };
