747c0427ca0cc95564d6907c127594dde3a1ecb2
irisesther <irisesther122@icloud.com>
2026-07-02 10:29:36 +0200

    ajout QR code

--- bit-components/eco-quiz/eco-quiz-mobile/eco-quiz-mobile.react-native-app.js
+++ bit-components/eco-quiz/eco-quiz-mobile/eco-quiz-mobile.react-native-app.js
@@ -1,3 +1,37 @@
 export default {
   name: 'eco-quiz-mobile',
-};
+};import React from 'react';
+import { View, Image, StyleSheet, Text } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Scannez pour partager l'application !</Text>
+      <Image 
+        source={require('../../../../frame.png')} 
+        style={styles.qrCode} 
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#d3f5ea', // Assorti au fond de votre app
+    padding: 20,
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#1fae87',
+    marginBottom: 20,
+    textAlign: 'center',
+  },
+  qrCode: {
+    width: 250,
+    height: 250,
+  },
+});
